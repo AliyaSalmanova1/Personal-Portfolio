1bf5f3dea4c106626858f3b0a9fa40ff0bfa267a
AliyaSalmanova1 <136350408+AliyaSalmanova1@users.noreply.github.com>
2024-05-08 11:08:04 -0400

    slider works without bugs now (but is not finished)

--- nextjs-dashboard/src/components/Skills.tsx
+++ nextjs-dashboard/src/components/Skills.tsx
@@ -1,79 +1,91 @@
-import React from "react"
-import Image from 'next/image'
+'use client';
+import { useState, useEffect } from 'react';
+// import Navigation from 'swiper';
+// import Pagination from 'swiper';
+// import A11y from 'swiper';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
 
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import Image from 'next/image';
 
 
 
 
 const Skills = () => {
 
-    interface SkillObjType{
-        technology: string;
-        icon: string
-    }
-
-    //https://icons8.com/icons/set/react
-
-    const skillsList: SkillObjType[]= [
-        {technology: "TypeScript/JavaScript",
-        icon: "/skillsIcons/tsIcon.png"},
-        {technology: "React",
-        icon: "/skillsIcons/react.png"},
-        {technology: "Redux",
-        icon: "/skillsIcons/reduxIcon.png"},
-        {technology: "Bootstrap",
-        icon: "/skillsIcons/bootstrap.png"},
-        {technology: "CSS",
-        icon: "/skillsIcons/cssIcon.png"},
-        {technology: "PostgreSQL",
-        icon: "/skillsIcons/postgreSQLIcon.png"},
-        {technology: "Firebase",
-        icon: "/skillsIcons/firebaseIcon.png"},
-        {technology: "MongoDB",
-        icon: "/skillsIcons/mongoDBIcon.png"}
-
-
-    ]
-    /*, "Python",
-        "React", "HTML5", "CSS", "SCSS/SASS", 
-        "Figma", "Redux/Redux Toolkit", "Bootstrap", "Next.js",
-        "Node", "Express", "SQL", "PostgreSQL", 
-        "NoSQL", "MongoDB", "Mongoose",
-        "Jest", "React Testing Library",
-        "SOAP APIs", "RESTful APIs", "Git/Github",
-        "webpack", "Electron", "Firebase"*/
-
-
-    return (
-        <div id="skills" className="h-screen py-20">
-            <h1 className="text-4xl">Skills</h1>
-            <div className="grid lg:grid-cols-4 sm:grid-cols-2 ">
-            {
-                skillsList.map(({technology, icon}) => {
-                    return (
-                        <div className="w-full flex justify-between items-center" >
-                            <span>{technology}</span>
-                            <Image 
-                            src={icon}
-                            width='68'
-                            height='68'
-                            alt="Picture of the author"
-                            />
-                        </div>
-                    )
-
-                })
-            }
-        </div>
-
-        </div>
-        
-        
-    )
-
-
-
+  interface SkillObjType{
+    technology: string;
+    icon: string
 }
+
+  const [skillsList, setSkillsList] = useState<SkillObjType[]>([])
+
+  
+  useEffect(() => {
+
+    setSkillsList([
+      {technology: "TypeScript/JavaScript",
+      icon: "/skillsIcons/tsIcon.png"},
+      {technology: "React",
+      icon: "/skillsIcons/react.png"},
+      {technology: "Redux",
+      icon: "/skillsIcons/reduxIcon.png"},
+      {technology: "Bootstrap",
+      icon: "/skillsIcons/bootstrap.png"},
+      {technology: "CSS",
+      icon: "/skillsIcons/cssIcon.png"},
+      {technology: "PostgreSQL",
+      icon: "/skillsIcons/postgreSQLIcon.png"},
+      {technology: "Firebase",
+      icon: "/skillsIcons/firebaseIcon.png"},
+      {technology: "MongoDB",
+      icon: "/skillsIcons/mongoDBIcon.png"}
+  
+  
+  ])
+
+  }, [])
+  
+  
+
+  return (
+    <div>
+      <h1>Custom Slider 1</h1>
+      <br />
+      <Swiper
+        modules={[Navigation, Pagination, A11y]}
+        slidesPerView={1}
+        spaceBetween={15}
+        breakpoints={{
+          480: { slidesPerView: 2 },
+          740: { slidesPerView: 3 },
+          1275: { slidesPerView: 4 },
+        }}
+      >
+        {skillsList.map(({technology, icon}) => (
+          <SwiperSlide
+            key={technology}
+            className='bg-red-200 !flex justify-center items-center'
+          >
+            <div className='border-2 border-blue-500 rounded-lg overflow-hidden w-[200px] h-[300px] flex justify-center items-center'>
+              <Image
+                src={icon}
+                width={150}
+                height={150}
+                alt={icon }
+              />
+            </div>
+          </SwiperSlide>
+        ))}
+
+  
+      </Swiper>
+    </div>
+  );
+};
 
 export default Skills;
