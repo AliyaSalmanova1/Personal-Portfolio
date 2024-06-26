e2e5e555c958c5203e44834fae989516d0f1a9c1
Aliya Salmanova <aliyasalmanova1@gmail.com>
2024-06-26 06:01:46 -0400

    changed navbar width to auto

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -108,7 +108,7 @@ const Skills = () => {
   
 
   return (
-    <div id="skills" className=" pt-[90px] px-5 max-w-[1300px] mx-auto
+    <div id="skills" className="pt-[90px] px-5 max-w-[1300px] mx-auto
     my-16">
       <p className="text-xl uppercase text-[#800020] mb-2">Skills</p>
       <h1 className="text-3xl font-semibold mb-2">Technologies I Use</h1>
